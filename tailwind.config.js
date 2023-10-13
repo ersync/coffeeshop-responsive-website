3408f72630d661b82a0fea6387644202560e0753
Sub Zero <121079771+ersync@users.noreply.github.com>
2023-10-13 10:51:18 +0330

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./public/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                "brown": {
+                    100: "#ECE0D1",
+                    300: "#DBC1AC",
+                    600: "#967259",
+                    900: "#634832"
+                }
+            },
+            boxShadow: {
+                "normal": "0px 1px 10px 0px rgba(0, 0, 0, 0.05);"
+            },
+            borderRadius: {
+                "4xl": "2rem"
+            },
+            fontFamily: {
+                "Dana": "Dana",
+                "DanaMedium": "Dana Medium",
+                "DanaDemiBold": "Dana DemiBold",
+                "MorabbaLight": "Morabba Light",
+                "MorabbaMedium": "Morabba Medium",
+                "MorabbaBold": "Morabba Bold",
+            }
+        },
+    },
+    plugins: [],
+}
+
